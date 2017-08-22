6fff160db32492611d533eabfbbcb527b8b7b01b
lixiaochichn@gmail.com <lixiaochichn@gmail.com>
2017-08-23 00:02:43 +0800

    updata0823

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,8 @@
 $(function () {
 
+
+
+
     let data = {
         client_id: '85fb7d41bc18a935b161',
         client_secret: '56f832136a7972de2085aa3f75f6a383a48f45ae'
@@ -14,6 +17,7 @@ $(function () {
 
         if ($userinput.val().length === 0) {
             userclear();
+            reposclear();
             return;
         };
 
@@ -34,12 +38,42 @@ $(function () {
                 userclear();
             });
 
+            $.ajax({
+                url: `https://api.github.com/users/${$userinput.val()}/repos`,
+                data
+            }).then(function (repos) {
+                showrepos(repos);
+            }).catch(function (repos) {
+                console.log(repos);
+                reposclear();
+            });
+
+            console.log(`A${data}`);
+
+
         }, 300);
 
+
+
+
+
+        function reposclear() {
+            $('.panel').html('');
+        };
 
         function userclear() {
             $('.usercard').html('');
         };
+
+        function showrepos(repos) {
+            console.log(repos);
+                $('.panel').html(`<p class="panel-heading"><span><i class="fa fa-bars" aria-hidden="true"></i></span> PUBLIC REPOS</p>`);
+            repos.map(function (repo) {
+                $('.panel').append(`<a  href="${repo.html_url}" class="panel-block">
+                <span class="panel-icon"><i class="fa fa-book"></i></span>
+                ${repo.name}<span class="star-icon">${repo.stargazers_count}<i class="fa fa-star"></i></span></a>`);
+            });
+        }
 
         function showuser(user) {
             console.log(user);
@@ -94,5 +128,19 @@ $(function () {
 
 
 
+
+    console.log(`      ___           ___           ___           ___     
+     /\\__\\         /\\__\\         /\\__\\         /\\  \\    
+    /:/  /        /:/  /        /::|  |        \\:\\  \\   
+   /:/__/        /:/  /        /:|:|  |         \\:\\  \\  
+  /::\\  \\ ___   /:/  /  ___   /:/|:|  |__       /::\\  \\ 
+ /:/\\:\\  /\\__\\ /:/__/  /\\__\\ /:/ |:| /\\__\\     /:/\\:\\__\\
+ \\/__\\:\\/:/  / \\:\\  \\ /:/  / \\/__|:|/:/  /    /:/  \\/__/
+      \\::/  /   \\:\\  /:/  /      |:/:/  /    /:/  /     
+      /:/  /     \\:\\/:/  /       |::/  /     \\/__/      
+     /:/  /       \\::/  /        /:/  /                 
+     \\/__/         \\/__/         \\/__/                  
+\n李晓驰(苏州)，应聘前端开发工程师 lixiaochichn@gmail.com
+`);
 
 })
